508870a28a26ea11c7a12f1a6788f980610d4c13
vlad-balance-pl <vladislav.tabakov@balance-pl.ru>
2022-08-11 21:47:49 +0300

    Base project structure with working resium viewer

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+
+const webpack = require('webpack');
+
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: config => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        CESIUM_BASE_URL: JSON.stringify('cesium'),
+      }),
+    );
+    return config;
+  }
+}
+
